d9a26ffb4a194b395a84bb1e69cfc56a0416abe3
GIANTGAMER <gianmito.gianmito@gmail.com>
2024-06-18 21:57:36 -0300

    primeiro commit no github

---
+++ aula 1.php
@@ -0,0 +1,26 @@
+<?php
+# Primeira aula 18/06/2024
+# Turma BG5000
+# Conteúdos: echo, comentários, variáveis
+
+echo "DIA<br>";
+
+$texto = "pequeno texto que vc está lendo sobre o livro do Menino Maluquinho";
+echo $texto;
+
+$outravariável = "<br>outro texto extremamente inútil para sua vida";
+
+echo $outravariável;
+
+echo "<br>cálculos matemágicos<br>";
+echo 1+1;
+echo "<br>";
+echo 5-3;
+echo "<br>";
+echo 10*20;
+echo "<br>";
+echo 10/2;
+
+
+
+?>
