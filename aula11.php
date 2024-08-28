820641dfeffcbc6986837651829c38880e9c9417
GIANTGAMER <gianmito.gianmito@gmail.com>
2024-08-27 21:58:26 -0300

    -

--- aula11.php
+++ aula11.php
@@ -2,4 +2,27 @@
 
 // validar cpf/cnpj via function separadas
 
+
+
+function MascaraCPF ($cpf){
+    $cpf = preg_replace('/[^0-9]/', '', $cpf);
+    return $cpf;
+}
+
+function ValidarCPF ($cpf){
+    $valido = "certo";
+    $erro = "cpf inválido";
+    if ($cpf = empty($cpf)){
+        return $erro;
+    }
+    if (strlen(MascaraCPF($cpf)) == 11) {
+        return $valido;
+    } else { 
+        return $erro;
+    }
+    
+
+}
+$cpf = "12356789110";
+echo ValidarCPF($cpf);
 ?>
---
+++ aula12.php
@@ -0,0 +1,27 @@
+<?php
+
+
+function addMask($TellCell, $mascara) {
+
+    $a =0;
+    $TelMask = "";
+    
+    for ($i=0; $i <= strlen($mascara); $i++) {
+
+        if ($mascara[$i] == '#') {
+            $TelMask .=  $TellCell[$a];
+            $a++;
+        } else {
+            $TelMask .= $mascara[$i];
+        }
+
+    }
+
+    return $TelMask;
+}
+$mascara = "(##) # ####-####";
+$TellCell = "54991595837";
+echo addMask($TellCell, $mascara);
+
+
+?>
