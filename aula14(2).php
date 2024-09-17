7ed5a4969fdab434359a86c8bb2c5650bde2a6e4
GIANTGAMER <gianmito.gianmito@gmail.com>
2024-09-17 19:13:47 -0300

    -

--- aula14(2).php
+++ aula14(2).php
@@ -4,7 +4,9 @@ class Banco {
     private $saldo = 1500;
     public $extrato;
     public $saldoInsuficiente= "Valor inserido inválido, verifique se possui alguma letra ou está vazio.";
+    public $valorinvalido= "Valor inserido invalido.";
     public $extratoInvalido= 0;
+
     public function saque ($valor){
         $padrao = "/[^0-9]/is";
         $valor = preg_replace($padrao, "", $valor);
@@ -18,22 +20,41 @@ class Banco {
         }
     }
 
-
     public function deposito ($valor){
         $padrao = "/[^0-9]/is";
         $valor = preg_replace($padrao, "", $valor);
 
         if (empty($valor)){
+            $this->extrato = $this->valorinvalido;
+        } elseif ($valor<0){
             $this->extrato = $this->saldoInsuficiente;
         } else {
             $this->extrato = $this->saldo + $valor;
         }
     }
 
+    public function extrato (){
+        $this->extrato = $this->saldo;
+    }
+
+    public function pix ($valor, $destino){
+        $padrao = "/[^0-9]/is";
+        $valor = preg_replace($padrao, "", $valor);
+
+        if (empty($valor)){
+            $this->extrato = $this->valorinvalido;
+        } elseif ($valor<0){
+            $this->extrato = $this->saldoInsuficiente;
+        } else {
+            $this->extrato = $this->saldo + $valor;
+        }
+    }
+
+
 }
 
 $banco = new Banco();
-$banco->saque("-100");
+$banco->deposito(0);
 echo $banco->extrato;
 
 ?>
